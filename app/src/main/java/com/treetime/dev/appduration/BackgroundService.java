9b918ab4e696fd4b3640f87a5173ecd6419c327d
ferrazsidney <ferrazsidney@gmail.com>
2021-12-18 19:37:08 -0300

    Verifica e exibe o nível da bateria

--- app/src/main/java/com/treetime/dev/appduration/BackgroundService.java
+++ app/src/main/java/com/treetime/dev/appduration/BackgroundService.java
@@ -4,7 +4,9 @@ import android.app.Service;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
@@ -12,7 +14,6 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,6 +25,9 @@ import static com.treetime.dev.appduration.MainActivity.WHATSAPP_COUNTER;
 public class BackgroundService extends Service {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+
+    int level, scale;
+    float batteryPct;
     public BackgroundService(){
 
     }
@@ -34,6 +38,11 @@ public class BackgroundService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         sharedPreferences = getSharedPreferences("App Duration", MODE_PRIVATE);
         editor = sharedPreferences.edit();
+
+        //Determinar o estado de carregamento atual da bateria
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        final Intent batteryStatus = this.registerReceiver(null, intentFilter);
+
         TimerTask detectApp = new TimerTask() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
@@ -58,7 +67,19 @@ public class BackgroundService extends Service {
 
                         editor.apply();
                         Log.d(TAG, usageStat.getPackageName() + " = " + usageStat.getTotalTimeInForeground());
+
+                        //Imprimir uma lista dos pacotes instalados e seus determinados tempos de uso
+                        if (!usageStat.getPackageName().toLowerCase().contains("com.android") &&
+                                !usageStat.getPackageName().toLowerCase().contains("com.google")) {
+                            System.out.println("TREETIME DEV: " + usageStat.getPackageName() + " = " + usageStat.getTotalTimeInForeground());
+                        }
                     }
+                    //determinando o nível da bateria
+                    level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                    scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                    batteryPct = level * 100 / (float)scale;
+                    System.out.println("TREETIME DEV: Nível da bateria - " + batteryPct);
+
                 }
             }
         };
